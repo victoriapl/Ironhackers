b601dd90ef6108cb2a69f2fdd200b1d1e717e534
VictoriaPl <victoriapp15@gmail.com>
2019-05-01 11:13:29 -0500

    Solved some issues with edit profile

--- routes/index.js
+++ routes/index.js
@@ -16,16 +16,17 @@ router.get('/editProfile/:id', isLogged, (req, res, next) => {
       res.render('editProfile', {user})
     })
     .catch(err => {
-      res.send(err)
+      console.log(err)
     })
 })
 
 router.post('/editProfile/:id', isLogged, uploadCloud.single('profileImg'), (req, res, next) => {
   const { id } = req.params
+  const correctLink = url => url.includes('https://') ? url : 'https://' + url 
   const { username, name, age, currentJob, twitter, linkedin, github, facebook, codewars, instagram } = req.body
-  User.findByIdAndUpdate(id, { $set: {username, name, age, currentJob, twitter, linkedin, github, facebook, codewars, instagram, profileImg: req.file ? req.file.secure_url : '' } }, { new: true })
+  User.findByIdAndUpdate(id, { $set: {username, name, age, currentJob, twitter: correctLink(twitter), linkedin: correctLink(linkedin), github: correctLink(github), facebook: correctLink(facebook), codewars: correctLink(codewars), instagram: correctLink(instagram), profileImg: req.file ? req.file.secure_url : '' } }, { new: true })
     .then(user => {
-    return res.redirect(`/profile/${user._id}`, user)
+    return res.redirect(`/profile/${user.id}`)
     })
     .catch(err => res.send(err))
 })
